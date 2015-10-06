6db93f1bbcd48c171f7212df23083ad431282b30
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2015-10-06 12:23:48 +0100

    Moved anchor tests from test_search.cpp to test_search_assertions.cpp.

--- test/test_search.cpp
+++ test/test_search.cpp
@@ -630,41 +630,6 @@ SCOPE_FIXTURE_CTOR(searchReturnValueTest2, STest, STest(R"(a+b)")) {
   SCOPE_ASSERT_EQUAL(27u, fixture.RetVal);
 }
 
-SCOPE_FIXTURE_CTOR(searchWhackATest, STest, STest(R"(\A)")) {
-  SCOPE_ASSERT(fixture.parsesButNotValid());
-}
-
-SCOPE_FIXTURE_CTOR(searchWhackAaTest, STest, STest(R"(\Aa)")) {
-  const char text[] = "aaabaacabbabcacbaccbbbcbccca";
-  fixture.search(text, text + 28, 0);
-  SCOPE_ASSERT_EQUAL(1u, fixture.Hits.size());
-  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
-}
-
-SCOPE_FIXTURE_CTOR(searchWhackZTest, STest, STest(R"(\Z)")) {
-  SCOPE_ASSERT(fixture.parsesButNotValid());
-}
-
-SCOPE_FIXTURE_CTOR(searchAWhackZTest, STest, STest(R"(a\Z)")) {
-  const char text[] = "aaabaacabbabcacbaccbbbcbccca";
-  fixture.search(text, text + 28, 0);
-  SCOPE_ASSERT_EQUAL(1u, fixture.Hits.size());
-  SCOPE_ASSERT_EQUAL(SearchHit(27, 28, 0), fixture.Hits[0]);
-}
-
-SCOPE_FIXTURE_CTOR(searchWhackAaWhackZTooLongTest, STest, STest(R"(\Aa\Z)")) {
-  const char text[] = "aaabaacabbabcacbaccbbbcbccca";
-  fixture.search(text, text + 28, 0);
-  SCOPE_ASSERT_EQUAL(0u, fixture.Hits.size());
-}
-
-SCOPE_FIXTURE_CTOR(searchWhackAaWhackZTest, STest, STest(R"(\Aa\Z)")) {
-  const char text[] = "a";
-  fixture.search(text, text + 1, 0);
-  SCOPE_ASSERT_EQUAL(1u, fixture.Hits.size());
-  SCOPE_ASSERT_EQUAL(SearchHit(0, 1, 0), fixture.Hits[0]);
-}
-
 /*
 SCOPE_FIXTURE_CTOR(hitCaching, STest, STest("[a-zA-Z]+ing")) {
   // 2011-05-06. This pattern causes Vm to continue buffering hits in the Matches vector until closeOut().
